287c68ca72df74248bc5eac7ecb7039ab3c16edb
Greison <grisoncastilla@gmail.com>
2025-05-02 20:26:19 -0500

    fix visual bug input

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -5,30 +5,33 @@
       class="absolute group-focus-within:-translate-y-4
       group-focus-within:text-amber-600
       transition ease-linear duration-300"
-      :class="{'-translate-y-4':text!==''}"
+      :class="{'-translate-y-4':!text==''}"
       :for="id"
-      :ref="id"
     >{{ name }}</label>
 
     <input
       class="border-b-2 border-gray-500 focus:border-amber-500 bg-transparent
             outline-none transition-all duration-300 w-full p-1
              "
-        :class="{'text-amber-600':text!==''}"
+      :class="{'text-amber-600':!text==''}"
       :id="id"
-      v-model="text"
       :type="type"
+      v-model="text"
+      ref="id"
     >
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+
+import { text } from '../composables/useId';
+
 defineProps({
   name:String,
   id: String,
   type: String,
+  
 });
 
-let text = ref("");
+
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,58 +5,52 @@
   import Card from '../components/Card.vue'
   import { ref } from 'vue';
   import {IonContent, IonInfiniteScroll, IonInfiniteScrollContent} from '@ionic/vue'
-
-  import {useCard} from '../composables/useCard'
-  import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
+  import { text } from '../composables/useId';
+  import { CapacitorBarcodeScanner, CapacitorBarcodeScannerAndroidScanningLibrary, CapacitorBarcodeScannerCameraDirection, CapacitorBarcodeScannerScanOrientation, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
   let balance = ref("↑ Consulta tu saldo ↑");
   let aux_id = ref("xxxxxxxxxx")
+  let cards =[
+    {
+      name:"Nombre",
+      id:120938
+    }
+  ]
 
+  const scanBarcode = async () => {
+    try {
+      const result = await CapacitorBarcodeScanner.scanBarcode({
+        hint: CapacitorBarcodeScannerTypeHint.ALL,
+        scanInstructions: "Please scan a barcode",
+        scanButton: true,
+        scanText: "Scan",
+        cameraDirection: CapacitorBarcodeScannerCameraDirection.BACK,
+        scanOrientation: CapacitorBarcodeScannerScanOrientation.ADAPTIVE,
+        android: {
+          scanningLibrary: CapacitorBarcodeScannerAndroidScanningLibrary.ZXING
+        }
+      });
+      text.value = result.ScanResult;
+      id.value= result.ScanResult;
+      aux_id.value = id.value;
+    } catch (error) {
+      if (error instanceof Error) {
+        id.value= "Error: " + error.message;
+      } else {
+        id.value= "Error: Unknown error";
+      }
+    }
+  };
 
-  const {collection, addCard} = useCard()
-
-  //console.log(cards)
-  //addCard(12309, 'Primera');
 
   function consult(){
-    console.log(id.value)
-    aux_id.value = id.value;
+    
+    
     balance.value = 3400;
+    text.value=12341
+    id.value = 12341
+    aux_id.value = id.value;
   }
-
-  async function startScan() {
-    if (!isPlatform('capacitor')) {
-      alert('Este escáner solo funciona en dispositivos móviles.')
-      return
-    }
-
-    // Solicita permisos de cámara
-    const status = await CapacitorBarcodeScanner.checkPermission({ force: true })
-    if (!status.granted) {
-      alert('Permiso de cámara denegado.')
-      return
-    }
-
-    // Oculta el fondo para mostrar la cámara detrás
-    CapacitorBarcodeScanner.hideBackground()
-
-    // Inicia el escaneo
-    const result = await CapacitorBarcodeScanner.startScan()
-
-    // Muestra nuevamente la app
-    CapacitorBarcodeScanner.showBackground()
-
-    if (result.hasContent) {
-      id.value = result.content
-    } else {
-      id.value = 'No se detectó ningún código.'
-    }
-
-    // Detiene el escaneo
-    BarcodeScanner.stopScan()
-  }
-
-  let cards = ref(collection)
 
 </script>
 
@@ -65,14 +59,14 @@
   <ion-content>
     <div class="bg-white w-full flex flex-col text-left ">
       <div class="flex h-20 w-full bg-amber-600 items-end"> 
-        <h1 class="text-white ml-4"> <b> Transcard</b></h1>
+        <h1 class="text-white ml-4"> <b> Transcard {{ text.value }}</b></h1>
       </div>
 
       <div class="flex flex-col p-4 text-gray-600">
         <span class="text-xl "> <b>Consultar Saldo</b></span>
         <div class="flex mt-4">
 
-          <div @click="startScan()">
+          <div @click="scanBarcode()">
             <svg  class="h-9 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M8 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V8M8 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V16M21 8V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H16M21 16V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H16M3 12H3.01M7.5 12H7.51M16.5 12H16.51M12 12H12.01M21 12H21.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
