68b432fe7a559761963cb6af657fb7a8371f1dba
unknown <grisoncastilla@gmail.com>
2025-09-30 14:27:53 -0500

    Transacard 2 coming soon

---
+++ src/components/buttons/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="sm:px-0 drop-shadow-xl ">
+        <div v-html="action"
+            class=" flex text-white place-content-center px-6 transition-all duration-200 
+            p-2 rounded-lg bg-{{color}}-500 group ease-linear
+            cursor-pointer"
+            :class="{ 'bg-{{color}}-800  scale-95': isActive }" 
+            @touchstart="activate()" 
+            @touchend="deactivate()"
+            @touchcancel="deactivate()">
+        </div>
+    </div>
+
+</template>
+
+<script>
+defineProps({
+    action: String,
+    color: String
+})
+
+import { ref } from 'vue';
+
+let isActive = ref(false);
+
+function activate() {
+    isActive.value = true;
+}
+
+function deactivate() {
+    isActive.value = false;
+}
+</script>
