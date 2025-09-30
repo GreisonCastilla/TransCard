788f234590e97dbe2b486069eef374b79591a4f4
unknown <grisoncastilla@gmail.com>
2025-09-29 22:34:30 -0500

    fix scan in web

--- src/components/ScanId.vue
+++ src/components/ScanId.vue
@@ -2,30 +2,29 @@
   <div class="flex flex-col p-4 text-gray-600">
     <span class="text-xl "> <b>Consultar Saldo</b></span>
     <div class="flex-col mt-4 space-y-4">
-      <InputText
-        id="id"
-        name="Número de tarjeta"
-        type="number"
-      ></InputText>
+      <InputText id="id" name="Número de tarjeta" type="number"></InputText>
 
-      <NormalButton
-        @click="consult()"
-        action='
+      <NormalButton @click="consult()" action='
             Consultar
 
             <svg class="h-5 ml-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 21L15.0001 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-            '
-      > </NormalButton>
+            '> </NormalButton>
 
       <ScanButton @click="ScanConsult()"></ScanButton>
     </div>
 
+    <CameraScanner v-model="text" v-model:open-modal="barcodeModalState" v-model:camera-details="cameraDetails"
+      v-model:raw-result="rawResult" />
+
   </div>
+
 </template>
 
 <script setup>
+import CameraScanner from './CameraScanner.vue';
+import { Capacitor } from '@capacitor/core';
 import {
   CapacitorBarcodeScanner,
   CapacitorBarcodeScannerAndroidScanningLibrary,
@@ -41,6 +40,25 @@ import { ref } from "vue";
 import getBalance from "../requests/transcaribeSondapay";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import { watch } from 'vue';
+
+const isWeb = Capacitor.getPlatform() === 'web'
+
+let barcodeModalState = ref(false)
+let cameraDetails = ref({})
+let rawResult = ref({})
+
+const barcodeScannedAudio = new Audio('./assets/barcode-scanned.mp3')
+
+watch(text, async (newText, oldText) => {
+  if (newText.length >= 10) {
+    id.value = text.value;
+    auxId.value = text.value;
+    console.log('entro')
+    consult()
+  }
+
+})
 
 function consult() {
   if (!id.value == "") {
@@ -72,10 +90,23 @@ function consult() {
   }
 }
 
-const ScanConsult = async () => {
-  await scanBarcode();
+/*const ScanConsult = async () => {
+  console.log(isWeb)
+  if (isWeb){
+    barcodeModalState = true;
+  }else{
+    
+    await scanBarcode();  
+  }
+  
   consult();
-};
+};*/
+
+
+
+const ScanConsult = () => {
+  barcodeModalState.value = true
+}
 
 const scanBarcode = async () => {
   let result = null;
@@ -94,8 +125,6 @@ const scanBarcode = async () => {
     });
 
     text.value = result.ScanResult;
-    id.value = text.value;
-    auxId.value = result.ScanResult;
   } catch (error) {
     if (error instanceof Error) {
       id.value = "Error: " + error.message;
@@ -105,13 +134,4 @@ const scanBarcode = async () => {
   }
 };
 
-let isActive = ref(false);
-
-function activate() {
-  isActive.value = true;
-}
-
-function deactivate() {
-  isActive.value = false;
-}
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    legacy()
+    legacy(),
+    mkcert()
   ],
   resolve: {
     alias: {
